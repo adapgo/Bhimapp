d498f32f85b033b92b84427b75a16897ee93c25f
ragini-coder <ragini@adstringo.in>
2019-07-22 11:50:07 +0530

    update

--- app/src/main/java/com/example/loginpage/MainActivity.java
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -10,13 +10,14 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
 
-    Button login;
+    Button btn_login;
         @Override
     protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.new_login);
-            login = (Button) findViewById(R.id.login);
-            login.setOnClickListener(new View.OnClickListener() {
+            btn_login =  findViewById(R.id.btn_login);
+
+            btn_login.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
 
--- app/src/main/java/com/example/loginpage/ShareApp.java
+++ app/src/main/java/com/example/loginpage/ShareApp.java
@@ -14,30 +14,30 @@ public class ShareApp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_app);
-        start = findViewById(R.id.start);
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+//        start = findViewById(R.id.start);
+//        start.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                Intent intent = new Intent(ShareApp.this, Multilingual.class);
+//                startActivity(intent);
+//            }
+//        });
 
-                Intent intent = new Intent(ShareApp.this, Multilingual.class);
-                startActivity(intent);
-            }
-        });
 
-
-        whatsaap = findViewById(R.id.whatsaap);
-
-        whatsaap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent whatsappIntent = new Intent(Intent.ACTION_SEND);
-                whatsappIntent.setType("text/plain");
-                whatsappIntent.setPackage("com.whatsapp");
-                whatsappIntent.putExtra(Intent.EXTRA_TEXT, "The text you wanted to share");
-                startActivity(Intent.createChooser(whatsappIntent, "Share Image"));
-            }
-        });
+//        whatsaap = findViewById(R.id.whatsaap);
+//
+//        whatsaap.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                Intent whatsappIntent = new Intent(Intent.ACTION_SEND);
+//                whatsappIntent.setType("text/plain");
+//                whatsappIntent.setPackage("com.whatsapp");
+//                whatsappIntent.putExtra(Intent.EXTRA_TEXT, "The text you wanted to share");
+//                startActivity(Intent.createChooser(whatsappIntent, "Share Image"));
+//            }
+//        });
 
 
     }
